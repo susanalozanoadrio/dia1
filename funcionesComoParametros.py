6ceb39266e84d5a8591268c745ac17313f1dd014
susanalozanoadrio <tuxaymoe@yahoo.es>
2019-07-03 17:13:53 +0200

    Areglo prueba1 y sumo funciones de primer nivel como parametros

---
+++ funcionesComoParametros.py
@@ -0,0 +1,18 @@
+#las funciones de primer nivel aceptan como paramentros otras funciones
+
+def normal(x):
+    return x
+
+def cuadrado(x):
+    return x * x
+
+def sumaTodos(limitTo, f):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += f(i)
+        
+    return resultado
+
+print(sumaTodos(100, normal))
+print(sumaTodos(3, cuadrado))
+        
--- prueba01.py
+++ prueba01.py
@@ -1,7 +1,7 @@
 def sumaTodos(limitTo):
     resultado = 0
     for i in range(0, limitTo+1):
-        resultado += 1
+        resultado += i
     
     return resultado
 
