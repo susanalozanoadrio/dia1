05760ecbbddc0d7ee0dae296876c41e9be21e0e8
susanalozanoadrio <tuxaymoe@yahoo.es>
2019-07-03 17:59:42 +0200

    funciones de primer nivel como resultado

---
+++ funcionesDePrimerNivel01.py
@@ -0,0 +1,52 @@
+#funciones de primer nivel con return
+
+def maxi(*l):
+    if len(l) == 0:
+        return 0
+    
+    m=l[0]
+    
+    for ix in range(1, len(l)):
+        if l[ix] > m:
+            m = l[ix]
+            
+    return m
+
+def mini(*l):
+    if len(l) == 0:
+        return 0
+    
+    m=l[0]
+    
+    for ix in range(1, len(l)):
+        if l[ix] < m:
+            m = l[ix]
+            
+    return m
+
+def media(*l):
+    if len(l) == 0:
+        return 0
+
+    suma = 0
+    for valor in l:
+        suma += valor
+        
+    return suma / len(l)
+
+funciones={
+    'max' : maxi,
+    'min' : mini,
+    'med' : media
+    }
+
+def returnF(nombre):
+    nombre = nombre.lower()
+    if nombre in funciones.keys():
+        return funciones[nombre]
+    
+    return None
+
+#returnF('max')(3, 8, 15, 9)
+#returnF('min')(3, 8, 15, 9)
+#returnF('med')(3, 8, 15, 9)
