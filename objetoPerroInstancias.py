73efc043da3a25460f06c9bc9da9acee9e911de9
susanalozanoadrio <tuxaymoe@yahoo.es>
2019-07-06 19:47:36 +0200

    probando como hacer instancias al objeto y a sus métodos

---
+++ objetoPerroInstancias.py
@@ -0,0 +1,20 @@
+class Perro():
+    def __init__(self, n, e, p):
+        self.nombre = n
+        self.edad = e
+        self.edad = p
+        
+    def ladrar(self):
+        print("Guau, Guau")
+        
+def ladrar():
+    print("No tengo perro")
+    
+    
+#Al poner esto en consola nos demuestra como se hacen las instancias a un objeto
+#y como se usan sus metodos
+#salchicho = Perro('Salchicho', 3 ,15)
+#ladrar()
+#salchicho.ladrar()
+
+
